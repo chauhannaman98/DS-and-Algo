8d134aa3194e73c1ffc6058b593e457fb429d4a6
Naman Chauhan <chauhannaman98@gmail.com>
2020-09-29 21:04:06 +0530

    added reverse_string

---
+++ Reverse String/reverse_string.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution  {
+    public:
+        string reverseString(string A) {
+            int start = 0;
+            int end = A.size()-1;
+
+            while(start<=end)   {
+                swap(A[start],A[end]);
+                start++;
+                end--;
+            }
+
+            // ****using stack****
+            // stack<char> st;
+            // for(int i=0; i<A.size(); i++)   {
+            //     // push every char inside stack
+            //     st.push(A[i]);
+            // }
+
+            // //pop every character
+            // for(int i=0; i<A.size(); i++)    {
+            //     A[i] = st.top();
+            //     st.pop();
+            // }
+
+            return A;   
+        }
+};
+
+int main()    {
+    string str = "hello";
+    Solution sol;
+    cout<<sol.reverseString(str);
+    return 0;
+}
