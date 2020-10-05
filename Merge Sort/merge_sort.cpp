00080123b00b47ba05575755973bb0de15af0243
Naman Chauhan <chauhannaman98@gmail.com>
2020-10-06 01:12:02 +0530

    updated with better solution

--- Merge Sort/merge_sort.cpp
+++ Merge Sort/merge_sort.cpp
@@ -1,58 +1,53 @@
 #include <iostream>
 using namespace std;
 
-void mergeArrays(int x[], int y[], int a[], int start, int end) {
-    int mid = (start+end)/2;
-    int i=end;
-    int j=mid+1;
-    int k=start;
-    while(i<=mid && j<=end) {
-        if(x[i] < y[j]) {
-            a[k] = x[i];
-            i++;
-            k++;
-        }
-        else    {
-            a[k] = y[j];
-            j++;
-            k++;
-        }
+void mergeArrays(int *a, int s, int e)    {
+    int mid = (s + e) / 2;
+    int i = s;
+    int j = mid + 1;
+    int k = s;
+    int temp[100];
+
+    while (i <= mid && j <= e)  {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
     }
-    while(i<=mid)   {
-        a[k] = x[i];
-        k++;
-        i++;
-    }
-    while(j<=end)   {
-        a[k] = y[i];
-        k++;
-        j++;
-    }
+
+    while (i <= mid)
+        temp[k++] = a[i++];
+    while (j <= e)
+        temp[k++] = a[j++];
+
+    // We need to copy all element to original arrays
+    for (int i = s; i <= e; i++)
+        a[i] = temp[i];
 }
 
-void mergeSort(int a[], int start, int end)   {
-    if(start>=end)  {
+void mergeSort(int a[], int start, int end) {
+    // Base case - 1 or 0 elements
+    if (start >= end)
         return;
-    }
-    int mid = (start+end)/2;
-    int x[100], y[100];
-    for(int i=0; i<=mid; i++)    {
-        x[i] = a[i];
-    }
-    for(int i=mid+1; i<=end; i++)    {
-        x[i] = a[i];
-    }
-    mergeSort(x, start, mid);
-    mergeSort(y, mid+1, end);
-    mergeArrays(x, y, a, start, end);
+
+    // Follow 3 steps
+    // 1. Divide
+    int mid = (start + end) / 2;
+
+    // Recursively the arrays - start,mid and mid+1,end
+    mergeSort(a, start, mid);
+    mergeSort(a, mid + 1, end);
+
+    // Merge the two parts
+    mergeArrays(a, start, end);
 }
 
-int main()    {
-    int arr[] = {1,5,2,6,5};
-    mergeSort(arr, 0, 4);
+int main()  {
+    int arr[] = {1,52,42,5,2,6,5,8,18,21,16,32,0};
+    mergeSort(arr, 0, (sizeof(arr)/sizeof(int))-1);
 
-    for(int i=0; i<5; i++) {
-        cout<<arr[i]<<" ";
+    for (int i = 0; i<(sizeof(arr)/sizeof(int)); i++) {
+        cout << arr[i] << " ";
     }
     return 0;
 }
